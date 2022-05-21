f235ad1c05a6b0fa2fb0218a16d45c00d2777710
JamieEllisson <jamieellisson@gmail.com>
2022-05-21 11:21:45 +0100

    added news

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import Slideshow from '../components/Slideshow'
-import { useRouter } from 'next/router'
+import router, { useRouter } from 'next/router'
 import { sanityClient } from '../lib/sanity.server'
 import { urlFor } from "../lib/sanity"
 import { Article } from '../typings'
@@ -20,13 +20,13 @@ function Home({ articles }:Props) {
         <Slideshow/>
         <section className='socials'>
           <ul className='socials_list'>
-            <a><FaTwitterSquare/></a>
-            <a><FaLinkedin/></a>
-            <a><FaFacebookSquare/></a>
-            <a><FaInstagram/></a>
-            <a><FaYoutube/></a>
-            <a><FaTwitch/></a>
-            <a><FaDiscord/></a>
+            <a href='#'><FaTwitterSquare/></a>
+            <a href='#'><FaLinkedin/></a>
+            <a href='#'><FaFacebookSquare/></a>
+            <a href='#'><FaInstagram/></a>
+            <a href='#'><FaYoutube/></a>
+            <a href='#'><FaTwitch/></a>
+            <a href='#'><FaDiscord/></a>
           </ul>
         </section>
         <section className='latest_news'>
@@ -35,11 +35,21 @@ function Home({ articles }:Props) {
             {articles.map(post =>(
               <Link key={post._id} href={`news/${post.slug.current}`}>
                 <div className='news_item'>
-                  <Image src={urlFor(post.mainImage).url()!} alt="" layout='intrinsic' objectFit='cover' width={1920} height={1080} loading="lazy"/>
+                  <div className='news_image_container'>
+                    <Image src={urlFor(post.mainImage).url()!} alt="" layout='intrinsic' objectFit='cover' width={1920} height={1080} loading="lazy"/>
+                  </div>                  
+                  <div className='news_details'>
+                    <div className='news_details_info'><h4>{post.categories[0].title}</h4><span className='dot'></span><h4 id='date'>{new Date(post._createdAt).toLocaleDateString("en-US",{month: 'long',day: 'numeric',year: 'numeric'})}</h4></div>
+                    <h2>{post.title}</h2>
+                  </div>
                 </div>
               </Link>
             ))}
           </div>
+          <button role='see_more' onClick={() => router.push('/news')}>See All News</button>
+        </section>
+        <section>
+          <div className='section_heading_container'><div className='pill'></div><h3>Recent Matches</h3></div>
         </section>
       </main>
     </>
