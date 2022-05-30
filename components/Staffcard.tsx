c8f17fdccdd835f250d0d8cb86e7ae9c66bdbf47
JamieEllisson <jamieellisson@gmail.com>
2022-05-30 19:58:12 +0100

    css cleanup

--- components/Staffcard.tsx
+++ components/Staffcard.tsx
@@ -1,19 +1,20 @@
 import Image from 'next/image'
 import { FaTwitter, FaLinkedin } from 'react-icons/fa'
+import styles from '../styles/Staffcard.module.css'
 
 function Staffcard(props: any) {
   return (
-    <div className='staffcard_container'>
-      <div className='staffcard'>
-        <div className='staffcard_image_container'>
+    <div className={styles.container}>
+      <div className={styles.staffcard}>
+        <div className={styles.image_container}>
           <Image src={props.image} alt='' layout='intrinsic' objectFit='cover' width={456} height={330}/>
         </div>
-        <div className='staffcard_info_container'>
+        <div className={styles.info_container}>
           <h3>{props.role}</h3>
           <h4>{props.name}</h4>
           <p>{props.bio1}</p>
           <p>{props.bio2}</p>
-          <div className='staffcard_info_socials_container'>
+          <div className={styles.info_socials_container}>
             <h6><a href={props.twitter}><FaTwitter/></a></h6>
             <h6><a href={props.linkedin}><FaLinkedin/></a></h6>
           </div>          
--- pages/api/submit-form.ts
+++
@@ -1,8 +0,0 @@
-export default function handler(req : any,res : any){
-    const body = req.body;
-
-    console.log(body)
-
-
-    res.status(200).json({data: `${body.name}`})
-}
